2004a5cfd7e61ca083e0aad72629cf28304a1c6e
Diksha Bajaj <47319191+Diksha86@users.noreply.github.com>
2019-10-22 12:37:18 +0000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,7 +2,7 @@ variable "region" {}
 variable "gcp_project" {}
 variable "credentials" {}
 variable "solrvpc" {}
-variable "sonarsub" {}
+variable "solrsub" {}
 // variable "elk_instance_name" {}
 variable "instance_name" {}
 variable "solr_instance_ip_name" {}
