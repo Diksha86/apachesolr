5dc1603cafdfa1bc80273d06f501129b4e4fc69a
Diksha Bajaj <47319191+Diksha86@users.noreply.github.com>
2019-10-22 14:27:08 +0000

    Update solr.sh

--- solr.sh
+++ solr.sh
@@ -1,8 +1,7 @@
-sudo apt install default-jdk
 sudo apt-get install wget -y
 sudo wget http://www-eu.apache.org/dist/lucene/solr/8.2.0/solr-8.2.0.tgz
 tar xzf solr-8.2.0.tgz solr-8.2.0/bin/install_solr_service.sh --strip-components=2
+sudo apt install default-jdk -y
 sudo bash ./install_solr_service.sh solr-8.2.0.tgz
 sudo systemctl start solr
 sudo systemctl status solr
-sudo su - solr -c "/opt/solr/bin/solr create -c mycol1 -n data_driven_schema_configs"
